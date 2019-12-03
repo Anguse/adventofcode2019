c3354c676f1190a6bc9c8869ef4bc9990ae93476
Harald <liljaharald@gmail.com>
2019-12-03 17:45:35 +0100

    Finished 3/12

--- 19-12-03/wire_twist.py
+++ 19-12-03/wire_twist.py
@@ -1,89 +1,113 @@
 import string
-
-def intcode_program(intcodes):
-    for i in range(0,len(intcodes),4):
-        if intcodes[i] == 1:
-            intcodes[intcodes[i+3]] = intcodes[intcodes[i+1]] + intcodes[intcodes[i+2]]
-        elif intcodes[i] == 2:
-            intcodes[intcodes[i+3]] = intcodes[intcodes[i+1]] * intcodes[intcodes[i+2]]
-        elif intcodes[i] == 99:
-            break
-    return intcodes
-
+from math import inf
 
 if __name__ == '__main__':
-    intersections_input = open('intersections.txt', 'r').read()
-    intersections = intersections_input.split('\n')
-    map = []
-    for i in range(100000):
-        part_map = []
-        for j in range(100000):
-            part_map.append(0)
-        map.append(part_map)
-    #_intcodes = list(map(int, crossings))
-    map[0][0] = -1
-    closest_intersection_mhd = 50000
-    for wire in intersections:
-        crossings = wire.split(',')
-        pos_x = 50000
-        pos_y = 50000
-        print(closest_intersection_mhd)
-        for movement in crossings:
-            #print(pos_x)
-            #print(pos_y)
+    wire_input = open('intersections.txt', 'r').read()
+    wires = wire_input.split('\n')[:-1]
+    closest_intersection_mhd = inf
+    paths = []
+    for wire in wires:
+        min_steps = inf
+        steps = 0
+        path = {}
+        moves = wire.split(',')
+        pos_x = 0
+        pos_y = 0
+        pos = (pos_x,pos_y)
+        for movement in moves:
             direction = movement[0]
             distance = int(movement[1:])
             if direction == 'R':
                 #right
-                print('right')
-                print(distance)
                 for i in range(distance):
                     pos_x += 1
-                    if map[pos_x][pos_y] == 1:
-                        # intersection
-                        map[pos_x][pos_y] = 2
-                        manhattan_distance = abs(pos_x-50000)+abs(pos_y-50000)
-                        if closest_intersection_mhd > manhattan_distance:
-                            closest_intersection_mhd = manhattan_distance
-                    elif map[pos_x][pos_y] == 0:
-                        map[pos_x][pos_y] = 1
+                    steps += 1
+                    pos = (pos_x, pos_y)
+                    for other_path in paths:
+                        try:
+                            intersect = other_path[pos]
+                            sum_steps = steps + intersect[1]
+                            manhattan_distance = abs(pos_x)+abs(pos_y)
+                            if sum_steps < min_steps:
+                                min_steps = sum_steps
+                            if closest_intersection_mhd > manhattan_distance:
+                                closest_intersection_mhd = manhattan_distance
+                        except KeyError:
+                            pass
+                    try:
+                        visited = path[pos]
+                        pass
+                    except KeyError:
+                        path[pos] = [True, steps]
             elif direction == 'L':
                 #left
                 for i in range(distance):
                     pos_x -= 1
-                    if map[pos_x][pos_y] == 1:
-                        # intersection
-                        map[pos_x][pos_y] = 2
-                        manhattan_distance = abs(pos_x-50000)+abs(pos_y-50000)
-                        if closest_intersection_mhd > manhattan_distance:
-                            closest_intersection_mhd = manhattan_distance
-                    elif map[pos_x][pos_y] == 0:
-                        map[pos_x][pos_y] = 1
+                    steps += 1
+                    pos = (pos_x, pos_y)
+                    for other_path in paths:
+                        try:
+                            intersect = other_path[pos]
+                            sum_steps = steps + intersect[1]
+                            manhattan_distance = abs(pos_x)+abs(pos_y)
+                            if sum_steps < min_steps:
+                                min_steps = sum_steps
+                            if closest_intersection_mhd > manhattan_distance:
+                                closest_intersection_mhd = manhattan_distance
+                        except KeyError:
+                            pass
+                    try:
+                        visited = path[pos]
+                        pass
+                    except KeyError:
+                        path[pos] = [True, steps]
             elif direction == 'D':
-                # down
+                #down
                 for i in range(distance):
                     pos_y -= 1
-                    if map[pos_x][pos_y] == 1:
-                        # intersection
-                        map[pos_x][pos_y] = 2
-                        manhattan_distance = abs(pos_x-50000)+abs(pos_y-50000)
-                        if closest_intersection_mhd > manhattan_distance:
-                            closest_intersection_mhd = manhattan_distance
-                    elif map[pos_x][pos_y] == 0:
-                        map[pos_x][pos_y] = 1
+                    steps += 1
+                    pos = (pos_x, pos_y)
+                    for other_path in paths:
+                        try:
+                            intersect = other_path[pos]
+                            sum_steps = steps + intersect[1]
+                            manhattan_distance = abs(pos_x)+abs(pos_y)
+                            if sum_steps < min_steps:
+                                min_steps = sum_steps
+                            if closest_intersection_mhd > manhattan_distance:
+                                closest_intersection_mhd = manhattan_distance
+                        except KeyError:
+                            pass
+                    try:
+                        visited = path[pos]
+                        pass
+                    except KeyError:
+                        path[pos] = [True, steps]
             elif direction == 'U':
-                # up
+                #up
                 for i in range(distance):
                     pos_y += 1
-                    if map[pos_x][pos_y] == 1:
-                        # intersection
-                        map[pos_x][pos_y] = 2
-                        manhattan_distance = abs(pos_x-50000)+abs(pos_y-50000)
-                        if closest_intersection_mhd > manhattan_distance:
-                            closest_intersection_mhd = manhattan_distance
-                    elif map[pos_x][pos_y] == 0:
-                        map[pos_x][pos_y] = 1
-        print(closest_intersection_mhd)
+                    steps += 1
+                    pos = (pos_x, pos_y)
+                    for other_path in paths:
+                        try:
+                            intersect = other_path[pos]
+                            sum_steps = steps + intersect[1]
+                            manhattan_distance = abs(pos_x)+abs(pos_y)
+                            if sum_steps < min_steps:
+                                min_steps = sum_steps
+                            if closest_intersection_mhd > manhattan_distance:
+                                closest_intersection_mhd = manhattan_distance
+                        except KeyError:
+                            pass
+                    try:
+                        visited = path[pos]
+                        pass
+                    except KeyError:
+                        path[pos] = [True, steps]
+        paths.append(path)
+    print(closest_intersection_mhd)
+    print(min_steps)
 
 
     
