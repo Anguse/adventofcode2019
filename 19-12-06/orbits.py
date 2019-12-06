a57051e21ce55101114b32f5eae0fc86af622292
Harald <liljaharald@gmail.com>
2019-12-06 20:26:01 +0100

    day6

--- 19-12-06/orbits.py
+++ 19-12-06/orbits.py
@@ -11,9 +11,35 @@ def count_orbits(orbits):
             planet = orbit_relationships[planet]
             count += 1
     return count
+
+def dist_2_san(orbits):
+    count = 0
+    orbit_relationships = {}
+    you_path = []
+    san_path = []
+    for orbit in orbits:
+        planet1,planet2 = orbit.split(')')
+        orbit_relationships[planet2] = planet1
+    # Get distance from COM to YOU and SAN, individually
+    for planet in orbit_relationships.keys():
+        if planet == "YOU":
+            while planet in orbit_relationships.keys():
+                planet = orbit_relationships[planet]
+                you_path.append(planet)
+        elif planet == "SAN":
+            while planet in orbit_relationships.keys():
+                planet = orbit_relationships[planet]
+                san_path.append(planet)
+    # Find where path intersects, the position of this planet is the distance to SAN
+    for planet in you_path:
+        if planet in san_path:
+            count += san_path.index(planet)
+            break
+        count += 1
+    return count
    
 if __name__ == '__main__':
     orbits_input = open('input.txt', 'r').read()
     orbits = orbits_input.split('\n')
-    count = count_orbits(orbits)
+    count = dist_2_san(orbits)
     print(count)
